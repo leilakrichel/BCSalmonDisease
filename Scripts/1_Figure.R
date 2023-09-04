86e87b770986ea8679ed5e0092e1144c008fc620
Leila Krichel <leila.krichel@mail.utoronto.ca>
2023-09-04 18:46:55 -0400

    Changes to Figure 1 script

--- Scripts/1_Figure.R
+++ Scripts/1_Figure.R
@@ -1,27 +1,52 @@
 # Figure 1: Aquaculture and commercial fisheries #
 
-## Load packages ##
+## Load packages ## 
 
 library(tidyverse)
 library(readxl)
 
-## Load the data ##
+## Load the data ## 
 
-# The excel file contains data for:
-## Commerically-caught salmon in BC over time from DFO 
-## Farmed salmon production in BC (from DFO) and in Canada (from FAO) over time 
-
-
-# global_farmed <- read.csv("Data/aquaculture_quantity.csv")
-# global_pacificcommercial
-# global_atlanticcommercial
+global_farmed <- read.csv("Data/2_GlobalAquaculture.csv") 
+global_pacificcommercial <- read.csv("Data/3_GlobalPacificCapture.csv") 
+global_atlanticcommercial <- read.csv("Data/4_GlobalAtlanticCapture.csv") 
 
 BC_farmed <- read_excel("Data/1_BCSalmon.xlsx", sheet = 1) # farmed atlantic salmon
 BC_commercial <- read_excel("Data/1_BCSalmon.xlsx", sheet = 2) # commercially-caught pacific salmon 
 
 ## Prepare the data ## 
 
-# Need to get numbers for farmed atlantic salmon in BC
+# Farmed salmon global 
+
+global_farmed <- global_farmed %>%
+  select(-seq(2, 146, 2))
+colnames(global_farmed) <- c("country", as.character(2021:1950))
+global_farmed <- global_farmed %>%
+  gather(key = "year", value = "quantity",-1) %>% 
+  group_by(year) %>% 
+  summarize(quantity = sum(quantity, na.rm = TRUE))
+
+# Global commercial pacific salmon capture 
+
+global_pacificcommercial <- global_pacificcommercial %>%
+  select(-seq(2, 146, 2))
+colnames(global_pacificcommercial) <- c("country", as.character(2021:1950))
+global_pacificcommercial <- global_pacificcommercial %>%
+  gather(key = "year", value = "quantity",-1) %>% 
+  group_by(year) %>% 
+  summarize(quantity = sum(quantity, na.rm = TRUE))
+
+# Global commercial atlantic salmon capture 
+
+global_atlanticcommercial <- global_atlanticcommercial %>% 
+  select(-seq(2, 146, 2)) 
+colnames(global_atlanticcommercial) <- c("country", as.character(2021:1950))  
+global_atlanticcommercial <- global_atlanticcommercial %>% 
+  gather(key = "year", value = "quantity", -1)%>% 
+  group_by(year) %>% 
+  summarize(quantity = sum(quantity, na.rm = TRUE))
+
+# BC farmed atlantic salmon
 
 BC_farmed <- BC_farmed %>%
   filter(year >= 1991) %>%
@@ -36,7 +61,35 @@ BC_farmed <- BC_farmed %>%
 
 ## Plot the data ##
 
+png("Plots/1_Figure.png")
+
+par(mfrow = c(1, 2))
+
 ### A plot ###
+
+# line for farmed atlantic salmon
+plot(x = global_farmed$year, y = global_farmed$quantity,
+     ylab = "Quantity (tonnes)", xlab = "Year",
+     type = "l")
+points(x = global_farmed$year, y = global_farmed$quantity,
+     pch = 0)
+
+# line for commercially-caught pacific salmon
+lines(x = global_pacificcommercial$year, y = global_pacificcommercial$quantity)
+points(x = global_pacificcommercial$year, y = global_pacificcommercial$quantity,
+       pch = 2)
+
+# line for commercially-caught atlantic salmon
+lines(x = global_atlanticcommercial$year, y = global_atlanticcommercial$quantity)
+points(x = global_atlanticcommercial$year, y = global_atlanticcommercial$quantity)
+
+# legend
+legend("topleft", 
+       #title = "BC quantities",
+       legend = c("Farmed Atlantic salmon", 
+                  "Commercially-caught Atlantic salmon",
+                  "Commercially-caught Pacific salmon"),
+       pch = c(0, 1, 2))
 
 ### B plot ###
 
@@ -60,7 +113,6 @@ legend("topleft",
                   "Commercially-caught Pacific salmon"),
        pch = c(0, 2))
 
-### Paste A + B plots together ###
-
+dev.off()
 
 ## END ##
